2260e41f0ed4e18f8773a9fb3222e044a1946cdc
sc2bat <izar_dero@naver.com>
2022-10-10 21:25:23 +0900

    first

---
+++ src/test/java/com/example/vscode/VscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
