5accdb4858d53547d8856fb8dbd2054962e44d66
sc2bat <izar_dero@naver.com>
2022-10-11 21:55:29 +0900

    update logging level add logback-spring.xml

--- src/main/java/com/example/vscode/config/SecurityConfig.java
+++ src/main/java/com/example/vscode/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -24,9 +26,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
-        httpSecurity.csrf(csrf -> csrf.disable());
-        return httpSecurity.build();
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable());
+        return http.build();
     }
+    
 }
